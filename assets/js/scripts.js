774203367c7321b2dc8ca6e49182d85398fecebe
ISLEM FOURATI <fourati.islem@outlook.fr>
2025-02-17 16:48:30 +0100

    HTML CSS done et début Js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,14 @@
 // alert("js est connecté");
+
+// page who we are
+team1 = document.getElementById("team-person-1");
+team2 = document.getElementById("team-person-2");
+team3 = document.getElementById("team-person-3");
+team4 = document.getElementById("team-person-4");
+
+// add class image-team
+// console.log(team);
+
+team1.addEventListener("mouseenter", function () {
+  team1.classList.add("image-team-1");
+});
