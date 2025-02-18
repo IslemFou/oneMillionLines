5dcb17bd8f9640ed366adf16aae7184ed0c7d081
ISLEM FOURATI <fourati.islem@outlook.fr>
2025-02-18 12:24:51 +0100

    js team done

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,34 @@
 // alert("js est connecté");
 
 // page who we are
+team = document.querySelectorAll(".team-person");
 team1 = document.getElementById("team-person-1");
 team2 = document.getElementById("team-person-2");
 team3 = document.getElementById("team-person-3");
 team4 = document.getElementById("team-person-4");
+job = document.querySelectorAll(".job");
 
-// add class image-team
-// console.log(team);
+function changeImage(element, imagePerson) {
+  element.classList.add(imagePerson);
+  element.style.transition = "all 0.5s";
+}
 
-team1.addEventListener("mouseenter", function () {
-  team1.classList.add("image-team-1");
-});
+function removeImage(element, imagePerson) {
+  element.classList.remove(imagePerson);
+}
+
+//
+
+for (let i = 0; i < team.length; i++) {
+  team[i].addEventListener("mouseover", function (event) {
+    changeImage(team[i], "image-team-" + (i + 1));
+    job[i].classList.add("text-white");
+  });
+}
+
+for (let i = 0; i < team.length; i++) {
+  team[i].addEventListener("mouseleave", function (event) {
+    removeImage(team[i], "image-team-" + (i + 1));
+    job[i].classList.remove("text-white");
+  });
+}
